16f1fb00b3d407bc01ea3df6ccfce33ee181e7f4
Samson Zhang <wwsamson@yahoo.com>
2022-07-05 17:00:26 -0400

    add tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 export default function Index() {
     return (
         <>
-            <h1>Hello world!</h1>
-            <p>This is a starter template you can use for Web Dev for Makers projects.</p>
-            <img src="/logo.png" alt="Web Dev for Makers Logo" />
+            <p>Hello world!</p>
         </>
     );
 }
